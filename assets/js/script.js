2c659532649ee10daed4f770f951903f09fa8183
rambolabi <rambolabi@gmail.com>
2016-02-25 20:46:33 +0100

    so it begins
    starting project xyz

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+var javaLink = "www.miauw.xyz/button";
+
+function myFunction() {
+    document.getElementById("psswrd").innerHTML = javaLink;
+}
+
+
+
+/*
+var email = document.getElementById('email');
+var movie = document.getElementById('movie');
+var wens = document.getElementById('wens');
+
+
+
+document.getElementById('formSanta').addEventListener('submit',function(e){
+    e.preventDefault();
+
+    console.log(email.value);
+    console.log(movie.value);
+    console.log(wens.value);
+
+
+});
+    */
